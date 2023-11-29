44ee2a0276a76df84a0f9952c283936fe199dfaa
ujwal <`ujwal.yangalwar@decimalpointanalytics.com>
2023-11-29 19:19:00 +0530

    url changes done

--- App/Services/GlobalApi.js
+++ App/Services/GlobalApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL='http://chatbot-eight-tau.vercel.app/api/bardapi';
+const BASE_URL='https://chatbot-eight-tau.vercel.app/api/bardapi';
 
 const getBardApi = (useMessage)=>axios.get(
     BASE_URL+"?ques="+useMessage
